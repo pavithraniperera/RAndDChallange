712f9a009e615240dc84d305a8bfa37944489285
pavithrani <pererapavithrani02@gmail.com>
2025-04-04 09:07:49 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/randdchallange/RAndDChallangeApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.randdchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RAndDChallangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RAndDChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/randdchallange/RAndDChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.randdchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RAndDChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
