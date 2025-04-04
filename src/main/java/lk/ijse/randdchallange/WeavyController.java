a673a4a337d863280f262f27e100aef2df6ea59d
pavithrani <pererapavithrani02@gmail.com>
2025-04-04 09:19:48 +0530

    Create wavyController class

---
+++ src/main/java/lk/ijse/randdchallange/WeavyController.java
@@ -0,0 +1,10 @@
+package lk.ijse.randdchallange;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class WeavyController {
+
+}
