10658a86e4eb10c12de213c5acae03a8ce76a865
pavithrani <pererapavithrani02@gmail.com>
2025-04-04 09:24:47 +0530

    create post request endpoint

--- src/main/java/lk/ijse/randdchallange/WeavyController.java
+++ src/main/java/lk/ijse/randdchallange/WeavyController.java
@@ -1,10 +1,26 @@
 package lk.ijse.randdchallange;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/users")
 public class WeavyController {
+    @Autowired
+    private WeavyService weavyServer;
+
+    @PostMapping("/create")
+    public String createUser(@RequestParam String name, @RequestParam String email) throws IOException {
+        return weavyServer.createUser(name, email);
+    }
+
+
+
+
 
 }
